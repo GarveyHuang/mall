c472ed2b8bae359ea7f3fa38d13f109e960ce547
GarveyHuang <ishuangjw@foxmail.com>
2021-10-16 23:16:26 +0800

    商品模块 - 本地缓存工具开发

---
+++ mall-product/src/main/java/com/shura/mall/component/LocalCache.java
@@ -0,0 +1,40 @@
+package com.shura.mall.component;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.shura.mall.domain.PmsProductParam;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: Garvey
+ * @Created: 2021/10/16
+ * @Description: 本地缓存管理工具，采用 LRU 淘汰策略
+ */
+@Component
+public class LocalCache {
+
+    private Cache<String, PmsProductParam> localCache = null;
+
+    @PostConstruct
+    private void init() {
+        localCache = CacheBuilder.newBuilder()
+                // 设置本地缓存容器的初始容量
+                .initialCapacity(10)
+                // 设置本地缓存容器的最大容量
+                .maximumSize(500)
+                // 设置写缓存后多少秒过期
+                .expireAfterWrite(60, TimeUnit.SECONDS)
+                .build();
+    }
+
+    public void setLocalCache(String key, PmsProductParam object) {
+        localCache.put(key, object);
+    }
+
+    public PmsProductParam get(String key) {
+        return localCache.getIfPresent(key);
+    }
+}
