33a2399820e4642ce7cb205a6d9f9e125408c1bf
GarveyHuang <ishuangjw@foxmail.com>
2021-10-17 09:20:14 +0800

    优惠券模块 - 完成项目基本配置

---
+++ mall-coupons/src/main/java/com/shura/mall/MallCouponsApplication.java
@@ -0,0 +1,17 @@
+package com.shura.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Garvey
+ * @Created: 2021/10/17
+ * @Description:
+ */
+@SpringBootApplication
+public class MallCouponsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCouponsApplication.class, args);
+    }
+}
