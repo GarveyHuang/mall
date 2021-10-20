07811f20a7a8b76b1f9946ad7557cfa3799f4c6b
GarveyHuang <ishuangjw@foxmail.com>
2021-10-20 14:14:46 +0800

    权限中心 - 完成权限中心项目基本配置信息

---
+++ mall-auth-center/src/main/java/com/shura/mall/MallAuthCenterApplication.java
@@ -0,0 +1,12 @@
+package com.shura.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAuthCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAuthCenterApplication.class, args);
+    }
+}
