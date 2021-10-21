d3c83333ee3e4714fcf8a4073c7f02159ee923c9
GarveyHuang <ishuangjw@foxmail.com>
2021-10-21 16:28:00 +0800

    搜索模块 - 完成项目基本配置

---
+++ mall-search/src/main/java/com/shura/mall/MallSearchApplication.java
@@ -0,0 +1,17 @@
+package com.shura.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Garvey
+ * @Created: 2021/10/21
+ * @Description:
+ */
+@SpringBootApplication
+public class MallSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSearchApplication.class, args);
+    }
+}
