5022d15c199e0e4b7722eff84b1059968bf2d6d5
GarveyHuang <ishuangjw@foxmail.com>
2021-10-21 15:18:06 +0800

    门户模块 - 完成项目基本配置，添加 MyBatis 配置

---
+++ mall-portal/src/main/java/com/shura/mall/config/MyBatisConfig.java
@@ -0,0 +1,16 @@
+package com.shura.mall.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Author: Garvey
+ * @Created: 2021/10/21
+ * @Description: MyBatis 配置类
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan({"com.shura.mall.mapper", "com.shura.mall.dao"})
+public class MyBatisConfig {
+}
