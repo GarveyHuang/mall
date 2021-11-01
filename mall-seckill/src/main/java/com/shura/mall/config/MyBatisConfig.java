d2d9c083e7709a78098f799a10cc9796d0bc9bc9
GarveyHuang <ishuangjw@foxmail.com>
2021-11-01 15:38:45 +0800

    添加 MyBatis 配置

---
+++ mall-seckill/src/main/java/com/shura/mall/config/MyBatisConfig.java
@@ -0,0 +1,14 @@
+package com.shura.mall.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: Garvey
+ * @Created: 2021/10/30
+ * @Description:
+ */
+@Configuration
+@MapperScan({"com.shura.mall.mapper", "com.shura.mall.dao"})
+public class MyBatisConfig {
+}
