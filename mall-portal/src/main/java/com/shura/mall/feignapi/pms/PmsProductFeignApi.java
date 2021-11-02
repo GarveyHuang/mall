5faa5280971a0a96ed39e91d45ef5f45e44ccaef
GarveyHuang <ishuangjw@foxmail.com>
2021-11-02 09:50:54 +0800

    远程调用商品服务 feign 接口封装

--- mall-portal/src/main/java/com/shura/mall/feignapi/pms/PmsProductFeignApi.java
+++ mall-portal/src/main/java/com/shura/mall/feignapi/pms/PmsProductFeignApi.java
@@ -2,6 +2,7 @@ package com.shura.mall.feignapi.pms;
 
 import com.shura.mall.common.api.CommonResult;
 import com.shura.mall.domain.FlashPromotionProduct;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,6 +13,7 @@ import java.util.List;
  * @Created: 2021/10/21
  * @Description:
  */
+@FeignClient(name = "mall-product", path = "/pms")
 public interface PmsProductFeignApi {
 
     @GetMapping(value = "/flashPromotion/getHomeSecKillProductList")
