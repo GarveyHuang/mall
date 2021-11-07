4d6d2bf24246702c76422f113ee83ed183abe17e
GarveyHuang <ishuangjw@foxmail.com>
2021-11-07 14:58:30 +0800

    分布式事务配置

---
+++ mall-seckill/src/main/java/com/shura/mall/config/TransactionConfig.java
@@ -0,0 +1,30 @@
+package com.shura.mall.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @Author: Garvey
+ * @Created: 2021/11/2
+ * @Description: 分布式事务配置
+ */
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfig {
+
+    @Bean
+    public PlatformTransactionManager txManager(final DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(final DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
